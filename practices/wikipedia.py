0df36fcb7f3a291b9e4f880cf99c3615d876e21f
Luo pengfei <luopengfei@dsl-187-168-3-106-dyn.prod-infinitum.com.mx>
2018-03-28 14:52:36 +0800

    change wikipedia.py note

--- practices/wikipedia.py
+++ practices/wikipedia.py
@@ -11,10 +11,12 @@ import ssl,re,datetime,random
 ssl._create_default_https_context = ssl._create_unverified_context
 random.seed(datetime.datetime.now())
 
-# 程序的主函数首先把起始页面 https://en.wikipedia.org/wiki/Kevin_Bacon 里的词条链接列表 (links 变量)设置成链接列表。
-# 然后用一个循环，从页面中随机找一个词条链接标签并抽 取 href 属性，
-# 打印这个页面链接，再把这个链接传入 getLinks 函数，重新获取新的链接
-# 列表。
+'''
+程序的主函数首先把起始页面 https://en.wikipedia.org/wiki/Kevin_Bacon 里的词条链接列表 (links 变量)设置成链接列表。
+然后用一个循环，从页面中随机找一个词条链接标签并抽 取 href 属性，
+打印这个页面链接，再把这个链接传入 getLinks 函数，重新获取新的链接
+列表。
+'''
 
 
 def get_links(article_url):
